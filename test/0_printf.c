462cfc48dc19b01e584cd1fa414380c93475e0f6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-23 00:24:38 +0100

    Update 0_printf.c

--- test/0_printf.c
+++ test/0_printf.c
@@ -3,32 +3,67 @@
 #include <stdarg.h>
 #include <string.h>
 
-char* itoa(int value, char* result, int base) {
+/**
+	
+ * Ansi C "itoa" based on Kernighan & Ritchie's "Ansi C"
 
-        if (base < 2 || base > 36) { *result = '\0'; return result; }
+	
+ * with slight modification to optimize for specific architecture:
+	
+ */
+	
+void strreverse(char* begin, char* end) {
+	
+	char aux;
+	while(end>begin)
+	aux=*end, *end--=*begin, *begin++=aux;
+	
+}
+	
+void itoa(int value, char* str, int base) {
+	
+	static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	char* wstr=str;
+	int sign;
+	div_t res;
+	
 
-        char* ptr = result, *ptr1 = result, tmp_char;
-        int tmp_value;
 
-        do
+	
+	// Validate base
+	
+	if (base<2 || base>35)
+        { 
+         *wstr='\0'; 
+         return;
+        }
+	// Take care of sign
+	
+	if ((sign=value) < 0) 
         {
-                tmp_value = value;
-                value /= base;
-                *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-        } while ( value );
-
-        if (tmp_value < 0) 
-                
-                *ptr++ = '-';
+                value = -value;
         }
-        *ptr-- = '\0';
-        while(ptr1 < ptr) {
-                tmp_char = *ptr;
-                *ptr--= *ptr1;
-                *ptr1++ = tmp_char;
-        }
-        return result;
-        }
+	
+	// Conversion. Number is reversed.
+	
+	do {
+	
+		res = div(value,base);
+	
+		*wstr++ = num[res.rem];
+	
+	}while(value=res.quot);
+	
+	if(sign<0) *wstr++='-';
+	
+	*wstr='\0';
+	
+	
+	// Reverse string
+	
+	strreverse(str,wstr-1);
+	
+}
 
 int _printf(const char *format, ...)
 {
