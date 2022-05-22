a7e2009d0a151c05e8936e88188bd5490a73fc2f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-23 00:26:23 +0100

    Update 0_printf.c

--- test/0_printf.c
+++ test/0_printf.c
@@ -26,25 +26,17 @@ void itoa(int value, char* str, int base) {
 	char* wstr=str;
 	int sign;
 	div_t res;
-	
-
-
-	
-	// Validate base
-	
+		
 	if (base<2 || base>35)
         { 
          *wstr='\0'; 
          return;
         }
-	// Take care of sign
 	
 	if ((sign=value) < 0) 
         {
                 value = -value;
         }
-	
-	// Conversion. Number is reversed.
 	
 	do {
 	
@@ -52,14 +44,11 @@ void itoa(int value, char* str, int base) {
 	
 		*wstr++ = num[res.rem];
 	
-	}while(value=res.quot);
+	}while(value==res.quot);
 	
 	if(sign<0) *wstr++='-';
 	
 	*wstr='\0';
-	
-	
-	// Reverse string
 	
 	strreverse(str,wstr-1);
 	
