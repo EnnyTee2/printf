4253a10552ec9921f6cbb9393ac3a58959ce2560
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-24 09:25:59 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,29 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+/** 
+ * struct defin - defines the operation
+ * @tr: argument
+ * @func: function
+ */
+
+typedef struct defin
+{
+        char *tr;
+        int (*func)(va_list);
+} defin_t;
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list arr_list);
+int (*get_func(char s))(va_list arr_list);
+int print_str(va_list arr_list);
+int print_dec(va_list arr_list);
+int print_int(va_list arr_numlist);
+int print_bin(va_list bin_list);
+int print_uns(va_list arr_list);
+int print_oct(va_list arr_list);
+int print_hex(va_list arr_list);
+
