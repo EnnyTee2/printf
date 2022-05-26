8493843a7f3652077c19e0b88a14ba36e48ac2dd
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-26 04:08:17 +0100

    Update main.h

--- main.h
+++ main.h
@@ -49,7 +49,7 @@ int prinnhexa(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
+int print_funct_id(const char *s, int index)
 unsigned int handl_buf(char *buffer, char s, unsigned int ibuffer);
 int print_buf(char *buf, unsigned int nbuf);
 char *fill_bin_arr(char *bin, long int int_in, int isneg, int limit);
