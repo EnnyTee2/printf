88be452ac75ee324a4d92eddf8e91cb090abc175
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-26 05:55:01 +0100

    Update printt_upx.c

--- printt_upx.c
+++ printt_upx.c
@@ -16,7 +16,7 @@ int print_uphx(va_list arguments, char *buf, unsigned int ibuf)
 	isnegative = 0;
 	if (int_input == 0)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
+		ibuf = buffer_handler(buf, '0', ibuf);
 		return (1);
 	}
 	if (int_input < 0)
@@ -25,16 +25,16 @@ int print_uphx(va_list arguments, char *buf, unsigned int ibuf)
 		isnegative = 1;
 	}
 	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	binary = fill_bin_arr(binary, int_input, isnegative, 32);
 	hexadecimal = malloc(sizeof(char) * (8 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 8);
+	hexadecimal = fill_hex_arr(binary, hexadecimal, 1, 8);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
 		if (hexadecimal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			ibuf = buffer_handler(buf, hexadecimal[i], ibuf);
 			count++;
 		}
 	}
