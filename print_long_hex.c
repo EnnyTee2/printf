b3d5e45d783f184f943d210cef27e6c7cdf4988d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-27 00:52:05 +0100

    compile errors corrected

--- print_long_hex.c
+++ print_long_hex.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * prinhhexa - prints a short decimal in hexadecimal
+ * prinlhex - prints a long decimal in hexadecimal
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
-int prinhhexa(va_list arguments, char *buf, unsigned int ibuf)
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf)
 {
-	short int int_input, i, isnegative, count, first_digit;
+	long int int_input, i, isnegative, count, first_digit;
 	char *hexadecimal, *binary;
 
-	int_input = va_arg(arguments, int);
+	int_input = va_arg(arguments, long int);
 	isnegative = 0;
 	if (int_input == 0)
 	{
@@ -25,10 +24,10 @@ int prinhhexa(va_list arguments, char *buf, unsigned int ibuf)
 		isnegative = 1;
 	}
 
-	binary = malloc(sizeof(char) * (16 + 1));
-	binary = fill_bin_arr(binary, int_input, isnegative, 16);
-	hexadecimal = malloc(sizeof(char) * (4 + 1));
-	hexadecimal = fill_hex_arr(binary, hexadecimal, 0, 4);
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_bin_arr(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = fill_hex_arr(binary, hexadecimal, 0, 16);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
 		if (hexadecimal[i] != '0' && first_digit == 0)
