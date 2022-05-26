f14b68beba05ac224a7819dda57017a869dcfa1c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-26 05:53:24 +0100

    Update printt_shrt_upx.c

--- printt_shrt_upx.c
+++ printt_shrt_upx.c
@@ -17,7 +17,7 @@ int printshupx(va_list arguments, char *buf, unsigned int ibuf)
 
 	if (int_input == 0)
 	{
-		ibuf = handl_buf(buf, '0', ibuf);
+		ibuf = buffer_handler(buf, '0', ibuf);
 		return (1);
 	}
 	if (int_input < 0)
@@ -27,9 +27,9 @@ int printshupx(va_list arguments, char *buf, unsigned int ibuf)
 	}
 
 	binary = malloc(sizeof(char) * (16 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 16);
+	binary = fill_bin_arr(binary, int_input, isnegative, 16);
 	hexadecimal = malloc(sizeof(char) * (4 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 4);
+	hexadecimal = fill_hex_arr(binary, hexadecimal, 1, 4);
 
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
@@ -37,7 +37,7 @@ int printshupx(va_list arguments, char *buf, unsigned int ibuf)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			ibuf = buffer_handler(buf, hexadecimal[i], ibuf);
 			count++;
 		}
 	}
