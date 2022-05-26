7ddcdb2ab9cf3e2bdfcd72bf3f0491f97f265bf8
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-05-26 03:56:29 +0100

    Update main.h

--- main.h
+++ main.h
@@ -44,7 +44,7 @@ int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinpint(va_list arguments, char *buf, unsigned int ibuf);
-int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinnocta(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhexa(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
